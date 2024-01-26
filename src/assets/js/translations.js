f19d388fdb2a85a105d32d658cf2107cfa85bdcf
Alexandre <alexandretavares@outlook.com>
2024-01-25 23:09:37 -0300

    Included support for more languages

--- src/assets/js/translations.js
+++ src/assets/js/translations.js
@@ -30,5 +30,53 @@ const translations = {
         "quote": `この布告のコピーは、バザルトの板にヒエログリフ、デモティック、ギリシャ語で刻まれ、第一、第二、第三階級の寺院に、永遠の神、プトレマイオスの像と並べて設置されます。`,
         "reference": "ロゼッタストーンに刻まれたプトレマイオス5世の布告、紀元前196年、ブリティッシュ・ミュージアム。",
         "direction": `vertical-rl`
+    },
+    "es": {
+        "code": "es",
+        "lang": "Español",
+        "title": "Decreto del Faraón Ptolomeo V inscrito en la Piedra Rosetta",
+        "quote": "Copias de este Decreto serán talladas en jeroglíficos, demótico y griego en losas de basalto y colocadas en los templos de primer, segundo y tercer orden junto a la estatua de Ptolomeo, el dios eterno.",
+        "reference": "Decretos de Ptolomeo V en la Piedra Rosetta, 196 a.C., Museo Británico.",
+        "direction": "ltr"
+    },
+    "fr": {
+        "code": "fr",
+        "lang": "Français",
+        "title": "Décret du Pharaon Ptolémée V inscrit sur la Pierre de Rosette",
+        "quote": "Des copies de ce Décret seront gravées en hiéroglyphes, démotique et grec sur des dalles de basalte et placées dans les temples de premier, deuxième et troisième ordre à côté de la statue de Ptolémée, le dieu vivant éternel.",
+        "reference": "Décrets de Ptolémée V sur la Pierre de Rosette, 196 av. J.-C., Musée Britannique.",
+        "direction": "ltr"
+    },
+    "ru": {
+        "code": "ru",
+        "lang": "Русский",
+        "title": "Декрет фараона Птолемея V, выгравированный на Розеттском камне",
+        "quote": "Копии этого Декрета будут вырезаны иероглифами, демотическим и греческим языками на базальтовых плитах и размещены в храмах первого, второго и третьего порядка рядом со статуей Птолемея, вечноживущего бога.",
+        "reference": "Декреты Птолемея V на Розеттском камне, 196 год до н.э., Британский музей.",
+        "direction": "ltr"
+    },
+    "zh": {
+        "code": "zh",
+        "lang": "中文",
+        "title": "刻在罗塞塔石碑上的托勒密五世法老的法令",
+        "quote": "这项法令的副本将以象形文字、民用文字和希腊语刻在玄武岩石板上，并放置在一、二、三级神庙中，与托勒密永生之神的雕像并列。",
+        "reference": "托勒密五世在罗塞塔石碑上的法令，公元前196年，大英博物馆。",
+        "direction": "ltr"
+    },
+    "hi": {
+        "code": "hi",
+        "lang": "हिन्दी",
+        "title": "फिरौन प्तोलेमी पंचम की रोजेटा पत्थर पर अंकित डिक्री",
+        "quote": "इस डिक्री की प्रतियां हाइरोग्लिफ़्स, डेमोटिक और ग्रीक में बेसाल्ट स्लैब पर कटी जाएंगी और पहले, दूसरे और तीसरे श्रेणी के मंदिरों में प्तोलेमी की मूर्ति के पास रखी जाएंगी, सदैव जीवित देवता।",
+        "reference": "रोजेटा पत्थर पर प्तोलेमी पंचम की डिक्री, 196 ईसा पूर्व, ब्रिटिश संग्रहालय।",
+        "direction": "ltr"
+    },
+    "sw": {
+        "code": "sw",
+        "lang": "Kiswahili",
+        "title": "Amri ya Farao Ptolemy V iliyochongwa kwenye Jiwe la Rosetta",
+        "quote": "Nakala za Amri hii zitachongwa kwa herufi za hieroglyph, demotic na Kigiriki kwenye ubao wa mawe ya basalt na kuwekwa kwenye hekalu za daraja la kwanza, la pili na la tatu pamoja na sanamu ya Ptolemy, mungu aishiye milele.",
+        "reference": "Amri za Ptolemy V kwenye Jiwe la Rosetta, 196 KK, Jumba la Makumbusho la Uingereza.",
+        "direction": "ltr"
     }
 }
