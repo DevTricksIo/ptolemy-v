8a6a939839490eb2be3a5f6b16a5aeedd082344d
Alexandre <alexandretavares@outlook.com>
2024-01-25 22:56:23 -0300

    Enhance 'Ptolemy V' project with image, CSS improvements, and additional languages
    - Added a photo of Ptolemy V to the page
    - Polished the CSS for a more refined appearance
    - Center-aligned the text to the right
    - Included support for additional languages on the page

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,57 +1,80 @@
+const translationsArray = Object.values(translations);
+
 const languageSelect = document.getElementById('language-select');
 const quoteText = document.querySelector('.quote-text');
 const quoteReference = document.querySelector('.quote-reference');
 
-function setLanguage(language) {
-    quoteText.textContent = quotes[language].quote;
-    quoteReference.textContent = quotes[language].reference;
+document.addEventListener('DOMContentLoaded', function () {
+
+    if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+        window.location.replace('/?lang=en');
+    }
+
+    let lang = detectLanguageFromURL();
+
+    document.title = translations[lang].title;
+    document.documentElement.setAttribute("lang", lang);
+
+    quoteText.textContent = translations[lang].quote;
+    quoteReference.textContent = translations[lang].reference;
+
+    populateOptions();
+    languageSelect.value = lang;
+
+
+    if(translations[lang].direction !== 'ltr'){
+        document.body.style.writingMode = translations[lang].direction;
+    } 
+});
+
+function populateOptions() {
+
+    var selectElement = document.getElementById('language-select');
+
+    translationsArray.forEach(function (language) {
+        var optionElement = document.createElement("option");
+        optionElement.value = language.code;
+        optionElement.textContent = language.lang;
+        selectElement.appendChild(optionElement);
+    });
+}
+
+function changeLanguageParam(lang) {
+    const currentUrl = window.location.href;
+    const url = new URL(currentUrl);
+    url.searchParams.set('lang', lang);
+    const newUrl = url.toString();
+
+    if (currentUrl === newUrl) {
+        return;
+    }
+    window.location.href = newUrl;
+}
+
+function detectLanguageFromURL() {
+    const currentUrl = window.location.href;
+    const url = new URL(currentUrl);
+    const lang = url.searchParams.get('lang');
+
+    if (lang) {
+        return lang;
+    } else {
+        return 'en';
+    }
 }
 
 languageSelect.addEventListener('change', () => {
     const selectedLanguage = languageSelect.value;
-    setLanguage(selectedLanguage);
+
+    const codes = translationsArray.map(i => i.code);
+
+    if (codes.includes(selectedLanguage)) {
+
+        const currentUrl = window.location.href;
+        const url = new URL(currentUrl);
+        url.searchParams.set('lang', selectedLanguage);
+        const newUrl = url.toString();
+
+        window.location.href = newUrl;
+    }
 });
-
-// function randomLanguage() {
-//     const languages = Object.keys(quotes);
-//     const randomLanguage = languages[Math.floor(Math.random() * languages.length)];
-//     setLanguage(randomLanguage);
-// }
-
-//setInterval(randomLanguage, 60000);
-
-
-/** original acima */
-
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    const language = navigator.language || navigator.userLanguage;
-
-
-    language_attributes
-
-    console.log(`O idioma do navegador é: ${language}`);
-
-    alert(language);
-
-
-});
-
-function setQuoteReference(text){
-    const quoteReference = document.querySelector('.quote-reference');
-    quoteReference.textContent = text;
-}
-
-function setQuote(text){
-    const quoteText = document.querySelector('.quote-text');
-    quoteText.textContent = text;
-}
-
-function setTitlePage(title){
-    document.documentElement.title = title;
-}
-
-function setLanguageAttributes(language){
-    document.documentElement.lang = language;
-}
---
+++ src/assets/js/translations.js
@@ -0,0 +1,34 @@
+const translations = {
+    "en": {
+        "code": `en`,
+        "lang": `English`,
+        "title": `Decree of Pharaoh Ptolemy V inscribed on the Rosetta Stone`,
+        "quote": `Copies of this Decree shall be cut in hieroglyphs, demotic, and Greek on basalt slabs and placed in the first, second, and third-order temples alongside the statue of Ptolemy, the ever-living god.`,
+        "reference": `Decrees of Ptolemy V on the Rosetta Stone, 196 B.C., British Museum.`,
+        "direction": `ltr`
+    },
+    "pt": {
+        "code": `pt`,
+        "lang": `Português`,
+        "title":  `Decreto do Faraó Ptolomeu V inscrito na Pedra de Roseta`,
+        "quote": `Este decreto será inscrito sobre uma estela de pedra nos caracteres sagrados e nativos e gregos e será erigida em cada um dos templos de primeiro, segundo e terceiro graus, ao lado da imagem do Rei Eterno.`,
+        "reference": `Decretos de Ptolomeu V na Pedra de Roseta, 196 a.C., Museu Britânico.`,
+        "direction": `ltr`
+    },
+    "ar": {
+        "code": `ar`,
+        "lang": `العربية`,
+        "title": `مرسوم الفرعون بطليموس الخامس المنقوش على حجر رشيدة`,
+        "quote": `سيتم قطع نسخ من هذا المرسوم بالهيروغليفية والديموتيك واليونانية على ألواح البازلت ووضعها في المعابده من الدرجة الأولى والثانية والثالثة بجوار تمثال بطليموس، إله الحياة الأبدية.`,
+        "reference": `مراسيم بطليموس الخامس على حجر رشيدة، 196 قبل الميلاد، المتحف البريطاني.`,
+        "direction": `rtl`
+    },
+    "ja": {
+        "code": `ja`,
+        "lang": `日本語`,
+        "title": `ロゼッタストーンに刻まれたファラオ・プトレマイオス5世の布告`,
+        "quote": `この布告のコピーは、バザルトの板にヒエログリフ、デモティック、ギリシャ語で刻まれ、第一、第二、第三階級の寺院に、永遠の神、プトレマイオスの像と並べて設置されます。`,
+        "reference": "ロゼッタストーンに刻まれたプトレマイオス5世の布告、紀元前196年、ブリティッシュ・ミュージアム。",
+        "direction": `vertical-rl`
+    }
+}
